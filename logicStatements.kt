a205df57c6fc6c30e03b39c08ea2888f0f7bb48f
pyroblaster <starac@thegame.com>
2017-12-10 20:26:33 +0100

    Logic Statements Kotlin
    Logic Statements

---
+++ logicStatements.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println(3>5) //is 3 greater than 5;false
+        println(3<5) //is 3 less than 5;true
+    println(3==5) //is 3 equal 5;false
+        println(3<=5) //is 3 less or equal 5;true
+    println(3>=5) //is 3 greater or equal 5;false
+        println(3!=5) //is 3 different from 5;true
+    println(3>5 && 3<10) //is 3 greater than 5 AND is 3 less than 10;false
+        println(3<5 && 3<10) //is 3 less than 5 AND is 3 less than 10;true
+    println(3>5 || 3<5) //is 3 greater than 5 OR is 3 less than 5;true
+        println(3>5 || 3>5) //is 3 greater than 5 OR is 3 greater than 5;false
+    println(3<5 || 3<5) //is 3 less than 5 OR is 3 less than 5;true
+        println(3<5 || 3>5) //is 3 less than 5 OR is 3 less than 5;true
+    var  istina:Boolean= true
+    println(!istina) //NOTistina;false
+    println(!true)
+}
