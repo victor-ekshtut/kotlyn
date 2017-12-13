432a4b97c6aeb0c6a6fbf77f2efa6a7d87efccfa
pyroblaster <starac@thegame.com>
2017-12-13 19:09:50 +0100

    When Kotlin
    When

--- When.kt
+++ When.kt
@@ -8,6 +8,7 @@ fun main(args: Array<String>) {
         0 -> println("X is 0.")
         10 -> println("EKS ES CEN")
         42 -> println("Answer!")
-    else -> println("x is not 0 or 10 or 42, it is $x.")
+        in 42..420 -> println("Between 42 and 420.")
+    else -> println("x is not 0 or 10 or 42..420, it is $x.")
     }
 }
