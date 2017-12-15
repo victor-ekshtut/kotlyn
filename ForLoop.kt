9da668ff2e7168a67e92d4d507ee98966baa329e
pyroblaster <starac@thegame.com>
2017-12-15 03:48:44 +0100

    For Loop Kotlin
    For Loop Kotlin

---
+++ ForLoop.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Reptilko on 12/15/2017.
+ */
+fun main(args: Array<String>) {
+    for(item in 0..10)
+    {
+    println(item) //it will print from 0 to 10
+    }
+    var five=5
+    for(item in 0..5){
+        println(five*item) //or without variable just println(5*item)
+    }
+}
