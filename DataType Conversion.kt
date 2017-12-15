0bf30511afe5b4a533f4d4b40903d71528ebd26c
pyroblaster <starac@thegame.com>
2017-12-15 04:10:53 +0100

    DataType Conversion Kotlin
    DataType Conversion

---
+++ DataType Conversion.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by Reptilko on 12/15/2017.
+ */
+fun main(args: Array<String>) {
+    var x:String="10"
+    var y:Int?
+    y=x.toInt()
+    println(y)
+
+    var z:String?
+    z=y.toString()
+    println(z)
+
+    var xDouble=5.47
+    var yInt:Int?
+    yInt=xDouble.toInt()
+    println(yInt)
+}
