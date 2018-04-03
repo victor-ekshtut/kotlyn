4b930f12cea048462e989bd80f2e5bfbf744cd54
pyroblaster <starac@thegame.com>
2018-04-03 18:31:03 +0200

    globalLocalVar

---
+++ globalLocalVar.kt
@@ -0,0 +1,17 @@
+/**
+ * Created by Reptilko on 1/13/2018.
+ */
+
+var fullname="Pyro Blaster"  //global var
+
+fun display(){
+    print(fullname)
+}
+
+fun main(args: Array<String>) {
+    var name="pyroblaster"  //local var
+
+    print("Name: " + name)
+
+    print(fullname)
+}
